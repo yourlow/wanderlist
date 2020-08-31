b30ab93460069772f9af3a902a95132ee08006a1
Brian Zhang <brian.zhang@uq.edu.au>
2020-08-31 15:48:07 +1000

    Added brian's routes

--- wanderlist/urls.py
+++ wanderlist/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('<int:id>/brian/', views.brian, name='brian'),
     path('<str:business_name>/add_business/', views.add_business, name='example'),
     path('<int:business_id>/get_business_name/', views.get_business_by_id, name='get'),
+    path('get_bucketlist_activities/<bucketlist_id>', views.get_bucketlist_activities, name='get_bucketlist_activities'),
+    path('get_rewards/<activity_id>', views.get_rewards, name='get_rewards'),
+    path('get_user_rewards/<user_id>/<redeemed>', views.get_user_rewards, name='get_user_rewards'),
+    path('get_specific_user_rewards/<user_id>/<reward_id>', views.get_specific_user_reward, name='get_specific_user_rewards'),
+    path('get_business/<id>', views.get_business, name='get_business'),
 ]
--- wanderlist/views.py
+++ wanderlist/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from .models import *
 
@@ -22,3 +22,30 @@ def add_business(request, business_name, business_password):
 def get_business_by_id(request, business_id):
     get_activity = Business.objects.all().filter(id=business_id)
     return HttpResponse(get_activity)
+
+def get_bucketlist_activities(request, bucketlist_id):
+    bucketlist_activities = BucketList_Activity.objects.filter(bucketlist_id=bucketlist_id).values('activity_id')
+    bucketlist_activities_list = list(bucketlist_activities)
+    return JsonResponse(bucketlist_activities_list, safe=False)
+
+def get_rewards(request, activity_id):
+    rewards = Reward.objects.filter(activity_id=activity_id).values('id')
+    rewards_list = list(rewards)
+    return JsonResponse(rewards_list, safe=False)
+
+def get_user_rewards(request, user_id, redeemed):
+    # work out how to do booleans
+    rewards = User_Rewards.objects.filter(user_id=user_id, redeemed=False).values('id', 'reward_id')
+    rewards_list = list(rewards)
+    return JsonResponse(rewards_list, safe=False)
+
+def get_specific_user_reward(request, user_id, reward_id):
+    # seems weird, returns different ids 
+    rewards = User_Rewards.objects.filter(user_id=user_id, reward_id=reward_id).values('id', 'reward_id')
+    rewards_list = list(rewards)
+    return JsonResponse(rewards_list, safe=False)
+
+def get_business(request, id):
+    business = Business.objects.filter(id=id).values('id','name')
+    business_list = list(business)
+    return JsonResponse(business_list, safe=False)
