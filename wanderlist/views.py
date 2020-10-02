b9d4fc940aec5c848f32a82b4329d219f3a2b686
Matthew Holloway <m.holloway@uqconnect.edu.au>
2020-10-02 14:32:00 +1000

    activity post/get working

--- wanderlist/views.py
+++ wanderlist/views.py
@@ -4,6 +4,9 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.template import loader
+from django.http import HttpResponse, JsonResponse
+
 
 def index(request):
     #return HttpResponse('Refer to onedrive Routes document for details on routes')
